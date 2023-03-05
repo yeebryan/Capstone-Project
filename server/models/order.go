168a2effdffa37ab8522b31a3ed72253f5ca5ac2
ZevQuek <chekwee97@gmail.com>
2023-03-06 00:14:17 +0800

    Order struct

--- server/models/order.go
+++ server/models/order.go
@@ -7,7 +7,7 @@ import (
 type Order struct {
 	ID     primitive.ObjectID `bson:"_id"`
 	Dish   *string            `json:"dish"`
-	Price  *float64           `json:"price""`
+	Price  *float64           `json:"price"`
 	Server *string            `json:"server"`
 	Table  *string            `json:"table"`
 }
